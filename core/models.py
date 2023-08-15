2743f244cd62ba94ef2302001094b1e909dc5aa5
harrison <harrisonpinchieri@gmail.com>
2023-08-15 13:22:06 -0300

    v0.1

---
+++ core/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from stdimage.models import StdImageField
+
+# SIGNALS
+from django.db.models import signals
+
+'''
+signals significa que podemos aplicar um sinal no modelo de dados 
+e falar "antes de vc inserir os dados no banco de dados, faça algo"
+ou "depois que vc inserir, faça algo".
+'''
+from django.template.defaultfilters import slugify
+
+"""
+cria um slug, que é o o proprio titula da página ou qualquer coisa que nós queremos separado por traços  para criar uma url válida
+"""
+
+
+class Base(models.Model):
+    criado = models.DateField('Data de Criação', auto_now_add=True)
+    modificado = models.DateField('Data da Atualização', auto_now=True)
+    ativo = models.BooleanField('Ativo?', default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Produto(Base):
+    nome = models.CharField('Nome', max_length=100)
+    preco = models.DecimalField('Preço', max_digits=8, decimal_places=2)
+    estoque = models.IntegerField('Estoque')
+    image = StdImageField('Imagem', upload_to='produtos', variations={'thumb': (124, 124)})
+    slug = models.SlugField('Slug', max_length=100, blank=True, editable=False)
+
+    def __str__(self):
+        return self.nome
+
+
+def produto_pre_save(signal, instance, sender, **kwargs):
+    instance.slug = slugify(instance.nome)
+
+
+signals.pre_save.connect(produto_pre_save, sender=Produto)
